de5360454947e0267f0d587407fd750f1863b5b1
Jade Yu <50040414+hygoni@users.noreply.github.com>
2020-01-03 18:25:38 +0900

    add prototype of register API

--- src/main/java/com/github/hygoni/dormitory/controller/APIController.java
+++ src/main/java/com/github/hygoni/dormitory/controller/APIController.java
@@ -1,14 +1,21 @@
 package com.github.hygoni.dormitory.controller;
 
-import org.springframework.stereotype.Controller;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/")
 public class APIController {
     @RequestMapping(value = "/register", method = RequestMethod.POST, headers = "Accept=application/json")
-    public String register(@RequestBody String payload) {
-        System.out.println(payload);
+    public String register(@RequestBody String payload) throws ParseException {
+        JSONParser parser = new JSONParser();
+        JSONObject json = (JSONObject) parser.parse(payload);
+        String id = (String) json.get("id");
+        String password = (String) json.get("password");
+        int buildingNumber = Integer.parseInt((String) json.get("buildingNumber"));
+        System.out.println(String.format("%s, %s, %d", id, password, buildingNumber));
         return payload;
     }
 }
