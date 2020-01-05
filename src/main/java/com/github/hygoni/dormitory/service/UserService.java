cf081e4728c8b850ffa07265e619f30449c6cd2d
Jade Yu <50040414+hygoni@users.noreply.github.com>
2020-01-05 15:40:31 +0900

    add user service

--- src/main/java/com/github/hygoni/dormitory/service/UserService.java
+++ src/main/java/com/github/hygoni/dormitory/service/UserService.java
@@ -1,4 +1,23 @@
 package com.github.hygoni.dormitory.service;
 
+import com.github.hygoni.dormitory.model.User;
+import com.github.hygoni.dormitory.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+    public User saveUser(User user){
+        return userRepository.save(user);
+    }
+
+    public User findUserById(String id){
+       return userRepository.findUserById(id);
+    }
+
+    public void deleteUserById(String id){
+        userRepository.deleteUserById(id);
+    }
 }
