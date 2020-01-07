fc70aedfb24321b97efbe1a9e7b3f3cbc9d53033
hygoni <9hrack@naver.com>
2020-01-07 17:56:11 +0900

    update SecurityConfiguration

--- src/main/java/com/github/hygoni/dormitory/config/SecurityConfiguration.java
+++ src/main/java/com/github/hygoni/dormitory/config/SecurityConfiguration.java
@@ -1,9 +1,13 @@
 package com.github.hygoni.dormitory.config;
 
+import com.github.hygoni.dormitory.controller.security.JwtAuthenticationFilter;
 import com.github.hygoni.dormitory.controller.security.JwtTokenProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
@@ -19,5 +23,18 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         return super.authenticationManagerBean();
     }
 
-
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .httpBasic().disable() //rest api는 기본 설정 안함.
+                .csrf().disable() //CSRF 보안도 rest api는 필요 없음
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                    .authorizeRequests()
+                        .antMatchers("/register").permitAll()
+                        .antMatchers("/write").hasRole("USER")
+                        .anyRequest().denyAll()
+                .and()
+                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class); //Jwt 필터를 UsernamePasswordAuthenticationFilter 앞에 적용해야 함.
+    }
 }
