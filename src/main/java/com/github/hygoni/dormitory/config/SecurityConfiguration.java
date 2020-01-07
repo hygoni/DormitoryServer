9628668afe369d256699478598817ff0023e9864
hygoni <9hrack@naver.com>
2020-01-07 11:00:30 +0900

    add SecurityConfiguration (리소스 권한 설정)

---
+++ src/main/java/com/github/hygoni/dormitory/config/SecurityConfiguration.java
@@ -0,0 +1,23 @@
+package com.github.hygoni.dormitory.config;
+
+import com.github.hygoni.dormitory.controller.security.JwtTokenProvider;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    public SecurityConfiguration(JwtTokenProvider jwtTokenProvider) {
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+
+}
