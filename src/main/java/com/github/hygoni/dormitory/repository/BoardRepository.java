752a59ef89196d54861c25feeb887f3fd6945ca4
hygoni <9hrack@naver.com>
2020-01-13 00:51:38 +0900

    bug fix

--- src/main/java/com/github/hygoni/dormitory/repository/BoardRepository.java
+++
@@ -1,9 +0,0 @@
-package com.github.hygoni.dormitory.repository;
-
-import com.github.hygoni.dormitory.model.Board;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BoardRepository extends JpaRepository<Board, String> {
-}
